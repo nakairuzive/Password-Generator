d95a40856ff55a4b5687a766ef9526263de54206
NakaiRuzive01 <nakii.ruzie@gmail.com>
2025-11-17 23:59:27 +0200

    Basic Functionality Completed
    HTML Skeleton is completed.
    CSS styling is halfway complete; the box needs to be centred.
    The JavaScript function is working

---
+++ index.js
@@ -0,0 +1,17 @@
+let password1El = document.querySelector("#password1")
+let password2El = document.querySelector("#password2")
+let btnGenerateEl = document.querySelector("#btn-generate")
+
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+
+btnGenerateEl.addEventListener("click", function(){
+    for(let j = 0; j < 15; ++j){
+        let letters = Math.floor(Math.random() * characters.length);
+        password1El.textContent += characters[letters];
+        password2El.textContent += characters[letters];
+    }
+})
+
